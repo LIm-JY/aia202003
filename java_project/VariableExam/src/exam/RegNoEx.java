f4efd5a52d224cb118a31a0c77fa200f3ea1e1fb
LimJY <lgrnsdk@naver.com>
2020-04-09 19:04:37 +0900

    Create RegNoEx.java

---
+++ java_project/VariableExam/src/exam/RegNoEx.java
@@ -0,0 +1,11 @@
+package exam;
+
+public class RegNoEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String regNo = ("123456-1234567");
+		System.out.print(regNo);
+	}
+
+}
