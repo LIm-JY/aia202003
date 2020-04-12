297c14390f4c6dc1710cde2fb8c8433427c2dc33
LimJY <lgrnsdk@naver.com>
2020-04-12 16:32:25 +0900

    과제

--- java_project/Cp3-Operator/src/op/Ex3.java
+++ java_project/Cp3-Operator/src/op/Ex3.java
@@ -5,12 +5,18 @@ public class Ex3 {
 	public static void main(String[] args) {
 		// 3+6, 3+6+9, 3+6+9+12 의 연산을 하는 프로그램 작성.
 		//단. 덧셈 연산의 횟수 최소화
-		int num=0;
-		for(int i=1; i<9; i++) {
+		int sum = 0;
+		
+		for(int i=3; i<15; i++) {
 		if(i%3==0){
-			num +=i;
+			sum += i;
+			if(sum > 3)
+			
+			
+			System.out.println(sum);
 		}
 	}
-		System.out.println(num);
+	
+	
 }
 }
--- java_project/Cp3-Operator/src/op/Ex4.java
+++ java_project/Cp3-Operator/src/op/Ex4.java
@@ -8,6 +8,15 @@ public class Ex4 {
 		//c=(128/4)*2 일 때
 		//a>b>c 가 참이면 true 아니면 false를 출력하는 프로그램 작성 
 
+		int a = ((25+5)+(36/4))-72*5;
+		int b = ((25*5)+(36-4)-71)/4;
+		int c = (128/4)*2;
+
+		if((a>b&&b>c) == true) {
+		System.out.println("참 입니다");
+		}else {
+		System.out.println("거짓 입니다.");
+		}
+		}
 	}
 
-}
