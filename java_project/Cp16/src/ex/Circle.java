9960875ce360cbabe7a1f57c28052fb179373eff
LimJY <lgrnsdk@naver.com>
2020-05-08 13:00:59 +0900

    dd

---
+++ java_project/Cp16/src/ex/Circle.java
@@ -0,0 +1,20 @@
+package ex;
+
+import java.io.Serializable;
+
+public class Circle implements Serializable{
+
+	int xPos;
+	int yPos;
+	double rad;
+	
+	public Circle(int xPos, int yPos, double rad) {
+		this.xPos=xPos;
+		this.yPos=yPos;
+		this.rad=rad;
+	}
+	void showCircleInfo() {
+	System.out.println(xPos);
+	System.out.println(rad);
+	}
+}
